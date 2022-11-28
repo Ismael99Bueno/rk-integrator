97f0f34eb92d85abcf263ff16e03dc67620acdab
Ismael Bueno <ismaelwno@gmail.com>
2022-11-28 20:29:48 +0100

    Added profile configurations for test and now quad tree gets updated only once per frame

--- premake5.lua
+++ premake5.lua
@@ -20,6 +20,12 @@ project "runge-kutta"
       defines { "NDEBUG" }
       runtime "Release"
       optimize "On"
+      removefiles "src/test.cpp"
+
+   filter "configurations:test"
+      defines { "DEBUG" }
+      runtime "Debug"
+      symbols "On"
 
    filter "configurations:debug-profile"
       defines { "DEBUG", "PERF" }
@@ -31,7 +37,8 @@ project "runge-kutta"
       runtime "Release"
       optimize "On"
 
-   filter "configurations:test"
-      defines { "DEBUG" }
+   filter "configurations:test-profile"
+      defines { "DEBUG", "PERF" }
       runtime "Debug"
       symbols "On"
+
