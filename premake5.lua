42b118e4867bba1efa3509507b318ac954d5c438
Ismael Bueno <ismaelwno@gmail.com>
2022-12-24 00:26:55 +0000

    Correctly enabled openmp across the pertinent projects and applied it to the collidion detection methods. Fixed a bug with the quad tree that caused infinite recursive calls

--- premake5.lua
+++ premake5.lua
@@ -20,7 +20,6 @@ project "runge-kutta"
       defines { "NDEBUG" }
       runtime "Release"
       optimize "On"
-      removefiles "src/test.cpp"
 
    filter "configurations:test"
       defines { "DEBUG" }
