bd5cf3a4226056df8adf677c04af74abf13a0465
Ismael Bueno <ismaelwno@gmail.com>
2023-03-27 16:22:51 +0100

    Target and obj dirs no longer need their project names appended

--- premake5.lua
+++ premake5.lua
@@ -5,7 +5,7 @@ project "rk-integrator"
    kind "StaticLib"
 
    targetdir("bin/" .. outputdir)
-   objdir("build/" .. outputdir .. "/%{prj.name}")
+   objdir("build/" .. outputdir)
 
    files {"src/**.cpp", "include/**.hpp"}
 
