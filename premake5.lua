63f3a002b0a9c9d3af9a0466995a4036f289831c
Ismael Bueno <ismaelwno@gmail.com>
2023-03-27 16:15:06 +0100

    Now bin/build folders are with respect the actual root folder

--- premake5.lua
+++ premake5.lua
@@ -4,8 +4,8 @@ project "rk-integrator"
    staticruntime "on"
    kind "StaticLib"
 
-   targetdir("../bin/" .. outputdir)
-   objdir("../build/" .. outputdir .. "/%{prj.name}")
+   targetdir("bin/" .. outputdir)
+   objdir("build/" .. outputdir .. "/%{prj.name}")
 
    files {"src/**.cpp", "include/**.hpp"}
 
