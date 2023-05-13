d5289b4cacf6c8864ef3f88d99e3da820f2dc227
Ismael Bueno <ismaelwno@gmail.com>
2023-05-13 22:56:10 +0200

    Added allocators to include path

--- premake5.lua
+++ premake5.lua
@@ -31,6 +31,7 @@ includedirs {
    "include",
    "%{wks.location}/debug-log-tools/include",
    "%{wks.location}/profile-tools/include",
+   "%{wks.location}/allocators/include",
    "%{wks.location}/vendor/yaml-cpp/include",
    "%{wks.location}/vendor/spdlog/include"
 }
